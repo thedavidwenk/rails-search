ce492c56e9b3fcbd8fcefcb31a93e0cd766a3b33
David Wenk <davidwenk@icloud.com>
2024-06-08 17:44:51 +0200

    add directors name into query

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,14 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all  
     if params[:query].present?
-      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
+      sql_query = <<~SQL
+        movies.title @@ :query
+        OR movies.synopsis @@ :query
+        OR directors.first_name @@ :query
+        OR directors.last_name @@ :query
+      SQL
+
+      @movies = @movies.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     end
   end
 end
