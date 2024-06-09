35d7fcc3842b98ad67f134c41454016b18415473
David Wenk <davidwenk@icloud.com>
2024-06-09 15:57:00 +0200

    experiment with pg_search amd multisearch

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all  
     if params[:query].present?
-      sql_query = <<~SQL
-        movies.title @@ :query
-        OR movies.synopsis @@ :query
-        OR directors.first_name @@ :query
-        OR directors.last_name @@ :query
-      SQL
-
-      @movies = @movies.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
     end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  # include PgSearch::Model
+
+  # pg_search_scope :search_by_title_and_synopsis,
+  # against: [ :title, :synopsis ],
+  # associated_against: {
+  #   director: [:first_name, :last_name]
+  # },
+
+  # using: {
+  #   tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  # }
+
 end
